c2c747e69c86aaddd63c69094192939255ee1f54
TacticalTechJay <gogojayvin923@gmail.com>
2021-06-02 21:23:50 -0700

    Something didn't seem right.

--- src/commands/assign.js
+++ src/commands/assign.js
@@ -8,38 +8,36 @@ class Assign extends Command {
             aliases: ['set'],
             description: 'Assign a language to a channel.'
         });
-        this.execute = this.exec;
-    }
-
-    async exec(message, args, prisma, guildCf) {
-        if (!message.member.permissions.has("manageChannels")) return message.channel.createMessage('You need `MANAGE_CHANNELS` permission to use this command.');
-        if (!args[1]) return message.channel.createMessage('The correct usage would be: `!assign #Channel <lang>`');
-        else if (args.length > 2) return message.channel.createMessage('Too many arguments provided.');
-        const op = langKeys.filter(a => a === args[1].toLowerCase())[0] || langKeys.filter(lang => languages[lang].name.toLowerCase() === args[1].toLowerCase())[0]
-        if (!op) return message.channel.createMessage('That\'s... not a supported language.');
-        if (!guildCf) guildCf = await prisma.guild.create({data: { id: message.guildID }});
-        if (guildCf.channels.filter(a => a.id === message.channelMentions[0])[0]) return message.channel.createMessage('This channel is already assigned a language.');
-        if (guildCf.channels.filter(a => a.lang === op)[0]) return message.channel.createMessage(`A channel already has that language. <#${guildCf.channels.filter(a => a.lang ===op)[0].id}>`);
-        try {
-            guildCf.channels.push({
-                id: message.channelMentions[0],
-                lang: op
-            });
-            await prisma.guild.update({
-                where: {
-                    id: message.guildID
-                },
-                data: {
-                    channels: {
-                        set: guildCf.channels
+        this.execute = async (message, args, prisma, guildCf) => {
+            if (!message.member.permissions.has("manageChannels")) return message.channel.createMessage('You need `MANAGE_CHANNELS` permission to use this command.');
+            if (!args[1]) return message.channel.createMessage('The correct usage would be: `!assign #Channel <lang>`');
+            else if (args.length > 2) return message.channel.createMessage('Too many arguments provided.');
+            const op = langKeys.filter(a => a === args[1].toLowerCase())[0] || langKeys.filter(lang => languages[lang].name.toLowerCase() === args[1].toLowerCase())[0]
+            if (!op) return message.channel.createMessage('That\'s... not a supported language.');
+            if (!guildCf) guildCf = await prisma.guild.create({data: { id: message.guildID }});
+            if (guildCf.channels.filter(a => a.id === message.channelMentions[0])[0]) return message.channel.createMessage('This channel is already assigned a language.');
+            if (guildCf.channels.filter(a => a.lang === op)[0]) return message.channel.createMessage(`A channel already has that language. <#${guildCf.channels.filter(a => a.lang ===op)[0].id}>`);
+            try {
+                guildCf.channels.push({
+                    id: message.channelMentions[0],
+                    lang: op
+                });
+                await prisma.guild.update({
+                    where: {
+                        id: message.guildID
+                    },
+                    data: {
+                        channels: {
+                            set: guildCf.channels
+                        }
                     }
-                }
-            });
-            return await message.channel.createMessage(`Assigned <#${message.channelMentions[0]}> the following language: ${languages[op].name}`);
-        } catch (e) {
-            console.error(e);
-            return message.channel.createMessage('Something went wrong!')
-        }
+                });
+                return await message.channel.createMessage(`Assigned <#${message.channelMentions[0]}> the following language: ${languages[op].name}`);
+            } catch (e) {
+                console.error(e);
+                return message.channel.createMessage('Something went wrong!')
+            }    
+        };
     }
 }
 
--- src/commands/unassign.js
+++ src/commands/unassign.js
@@ -6,31 +6,30 @@ class Unassign extends Command {
             aliases: ['unset', 'clear'],
             description: 'Unassigns a channel\'s language.'
         })
-        this.execute = this.exec;
-    }
-    async exec(message, args, prisma, guildCf) {
-        if (!message.member.permissions.has("manageChannels")) return message.channel.createMessage('You need `MANAGE_CHANNELS` permission to use this command.');
-        if (!message.channelMentions[0] || !args[0]) return message.channel.createMessage('No channel specified.');
-        else if (args.length > 1) return message.channel.createMessage('Too many arguments provided');
-        if (!guildCf || !guildCf?.channels[0]) return message.channel.createMessage('No channels assigned.');
-        const oldChannels = guildCf.channels;
-        guildCf.channels = guildCf.channels.filter(channel => channel.id !== message.channelMentions[0]);
-        try {
-            await prisma.guild.update({
-                where: {
-                    id: message.guildID
-                },
-                data: {
-                    channels: {
-                        set: guildCf.channels
+        this.execute = async (message, args, prisma, guildcf) => {
+            if (!message.member.permissions.has("manageChannels")) return message.channel.createMessage('You need `MANAGE_CHANNELS` permission to use this command.');
+            if (!message.channelMentions[0] || !args[0]) return message.channel.createMessage('No channel specified.');
+            else if (args.length > 1) return message.channel.createMessage('Too many arguments provided');
+            if (!guildCf || !guildCf?.channels[0]) return message.channel.createMessage('No channels assigned.');
+            const oldChannels = guildCf.channels;
+            guildCf.channels = guildCf.channels.filter(channel => channel.id !== message.channelMentions[0]);
+            try {
+                await prisma.guild.update({
+                    where: {
+                        id: message.guildID
+                    },
+                    data: {
+                        channels: {
+                            set: guildCf.channels
+                        }
                     }
-                }
-            });
-            return await message.channel.createMessage(`Unassigned <#${message.channelMentions[0]}> | \`${oldChannels.find(ch => ch.id === message.channelMentions[0]).lang}\``);
-        } catch (e) {
-            console.error(e);
-            return message.channel.createMessage('Something went wrong! Try again later.');
-        }
+                });
+                return await message.channel.createMessage(`Unassigned <#${message.channelMentions[0]}> | \`${oldChannels.find(ch => ch.id === message.channelMentions[0]).lang}\``);
+            } catch (e) {
+                console.error(e);
+                return message.channel.createMessage('Something went wrong! Try again later.');
+            }    
+        };
     }
 }
 export default Unassign;
