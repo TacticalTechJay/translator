a68321e7377d85ca6e2570840f0471e9061f1155
TacticalTechJay <gogojayvin923@gmail.com>
2021-06-02 22:13:13 -0700

    What?

--- src/commands/help.js
+++ src/commands/help.js
@@ -13,7 +13,8 @@ class Help extends Command {
                 message.channel.client.commands.forEach(e => res += `${guildCf?.prefix || process.env.PREFIX}${e.label} - ${e.description}\n`);
                 return message.channel.createMessage(`Here are a list of commands:\n${res}`);
             } else if (args[0]) {
-                x = message.channel.client.commands.get(args[0].toLowerCase())
+                const x = message.channel.client.commands.get(args[0].toLowerCase())
+                console.log(x);
                 return x ? message.channel.createMessage(`${guildCf?.prefix || process.env.PREFIX}${x.label} - ${x.description}${x.aliases ? `\n${x.aliases.join(', ')}` : ''}\n\nUsage:${guildCf?.prefix || process.env.PREFIX}${x.label} ${x.usage}`) : message.channel.createMessage('That is not a valid command.')
             } else return message.channel.createMessage('How did you get here?');
         }
