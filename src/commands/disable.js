f84d785c2b46b471ad73c4f7df19573790baaae4
TacticalTechJay <gogojayvin923@gmail.com>
2021-06-17 10:37:54 -0700

    It worked!

--- src/commands/disable.js
+++ src/commands/disable.js
@@ -11,14 +11,13 @@ class Disable extends Command {
             if (!message.member.permissions.has("manageChannels")) return message.channel.createMessage('You need `MANAGE_CHANNELS` permission to use this command.');
             if (!args[0] && !message.channelMentions[0]) return message.channel.createMessage('No channel specified.');
             else if (!message.channel.guild.channels.has(message.channelMentions[0] || args[0])) {
-                console.log('Hello! You made it here!\n' + guildCf.channels.includes);
                 if (guildCf.channels.includes(message.channelMentions[0] || args[0])) {
                     guildCf.channels = guildCf.channels.filter(channel => channel !== (message.channelMentions[0] || args[0]))
                     await prisma.guild.update({
                         where: { id: message.guildID },
                         data: guildCf
                     })
-                    return message.channel.send('I have done a hard remove of that channel due to it either no longer existing or I am unable to read messages within that channel.')
+                    return message.channel.createMessage('I have done a hard remove of that channel due to it either no longer existing or I am unable to read messages within that channel.')
                 } else return message.channel.createMessage('⚠️ Invalid channel! ⚠️');
             }
             else if (args.length > 1) return message.channel.createMessage('Too many arguments provided');
