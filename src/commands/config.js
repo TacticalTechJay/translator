bd7a8226783579bd90700791a46aa2f78adc1b22
TacticalTechJay <gogojayvin923@gmail.com>
2021-06-19 22:21:00 -0700

    Copy and pasted from development branch.

---
+++ src/commands/config.js
@@ -0,0 +1,40 @@
+import { Command } from "eris";
+import { table } from 'table';
+
+class Config extends Command {
+    constructor() {
+        super('config', 'null', {
+            aliases: ['configuration', 'check'],
+            description: 'Check your guild\'s setup.'
+        })
+        this.execute = async (message, args, prisma, guildCf) => {
+            if (!guildCf) guildCf = await prisma.guild.create({ data: {id: message.guildID} })
+            const x = table([
+                ['GuildID', 'Channels', 'Languages', 'Prefix'],
+                [guildCf.id, `${guildCf.channels?.map(x => x.id)}`, `${guildCf.channels?.map(x => languages[x.lang].name).join('\n') || 'None.'}`, `${guildCf.prefix || process.env.PREFIX}`]
+            ], {
+                border: {
+                    topBody: `─`,
+                    topJoin: `┬`,
+                    topLeft: `┌`,
+                    topRight: `┐`,
+                
+                    bottomBody: `─`,
+                    bottomJoin: `┴`,
+                    bottomLeft: `└`,
+                    bottomRight: `┘`,
+                
+                    bodyLeft: `│`,
+                    bodyRight: `│`,
+                    bodyJoin: `│`,
+                
+                    joinBody: `─`,
+                    joinLeft: `├`,
+                    joinRight: `┤`,
+                    joinJoin: `┼`
+                }
+            });
+            return message.channel.createMessage(`\`\`\`\n${x}\`\`\``);
+        }
+    }
+}
